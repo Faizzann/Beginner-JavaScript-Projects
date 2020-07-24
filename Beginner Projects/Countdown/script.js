04cca9d77787a8b1e3dddf16601445dad713d914
Abdul Kader <63424561+abdulkaderm36@users.noreply.github.com>
2020-07-24 20:00:06 +0400

    Add files via upload

--- Beginner Projects/Countdown/script.js
+++ Beginner Projects/Countdown/script.js
@@ -1,47 +1,64 @@
-do{
-    var inputTime = prompt("Enter Time (day:hour:minute:second):");
-    var timeList = inputTime.split(":");
-}while(inputTime == "" || timeList.length < 4);
 
-var day = parseInt(timeList[0]);
-var hour = parseInt(timeList[1]);
-var minute = parseInt(timeList[2]);
-var second = parseInt(timeList[3]);
+var day = 0;
+var hour = 0;
+var minute = 0;
+var second = 0;
+var interval;
 
-function countdownStarter(days=0, hours=0, minutes=0, seconds=0){
-    document.querySelector(".day h3").innerHTML = days;
-    document.querySelector(".hour h3").innerHTML = hours;
-    document.querySelector(".minute h3").innerHTML = minutes;
-    document.querySelector(".second h3").innerHTML = seconds;
+document.getElementById("start").addEventListener("click", function(){
+    day = document.getElementById("days").value;
+    hour = document.getElementById("hours").value;
+    minute = document.getElementById("minutes").value;
+    second = document.getElementById("seconds").value;
+
+    document.querySelector(".user-input-container").setAttribute("class", "hide");
+    interval = setInterval(countdown,1000);
+})
+
+document.getElementById("days").addEventListener("blur", valueChecker);
+document.getElementById("hours").addEventListener("blur", valueChecker);
+document.getElementById("minutes").addEventListener("blur", valueChecker);
+document.getElementById("seconds").addEventListener("blur", valueChecker);
+
+
+function valueChecker(){
+    if(parseInt(this.value) > parseInt(this.max)){
+        this.value = this.max;
+    }
+    else if(parseInt(this.value) < parseInt(this.min)){
+        this.value = this.min;
+    }
+
+    if(this.value == ""){
+        this.value = 0;
+    }
 }
 
-countdownStarter(day,hour,minute,second);
-
-var interval = setInterval(function(){
-    second--;
+function countdown(){
     document.querySelector(".day h3").innerHTML = day;
     document.querySelector(".hour h3").innerHTML = hour;
     document.querySelector(".minute h3").innerHTML = minute;
     document.querySelector(".second h3").innerHTML = second;
 
-    if(second == 0 && minute == 0 && hour == 0 && day == 0){
+    if(second <= 0 && minute == 0 && hour == 0 && day == 0){
         clearInterval(interval);
         alert("Time Over");
         location.reload();
     }
-    else if(second == 0 && minute > 0){
+    else if(second <= 0 && minute > 0){
         second = 60;
         minute--;
     }
-    else if(second == 0 && minute == 0 && hour > 0){
+    else if(second <= 0 && minute <= 0 && hour > 0){
         second = 60;
         minute = 59;
         hour--;
     }
-    else if(second == 0 && minute == 0 && hour == 0 && day > 0){
+    else if(second <= 0 && minute <= 0 && hour <= 0 && day > 0){
         second = 60;
         minute = 59;
         hour = 23;
         day--;
     }
-},1000);
+    second--;
+}
